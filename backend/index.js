b133d43cd907b5e43ee3225a95a706ecab395a38
Sumit262601 <skr.04088s@gmail.com>
2024-07-17 11:46:24 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const connectToMongo = require('./db');
+const express = require('express')
+const cors = require('cors')
+
+require('dotenv').config();
+
+connectToMongo();
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+// APIs
+app.use('/api/auth/', require('./routes/auth'))
+app.use('/api/notes/', require('./routes/notes'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import NoteContext from "./noteContext";
+
+const NoteState = (props) => {
+    const host = "http://localhost:5000";
+    const notesInitial = []
+    const [notes, setNotes] = useState(notesInitial)
+
+    // GET all notes
+    const getNotes = async () => {
+        // ADD API Call
+        const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                //auth-token when client login in iNotebook
+                "auth-token": localStorage.getItem('token')
+            },
+        });
+        const json = await response.json();
+        setNotes(json)
+    }
+
+    // Add notes
+    const addNote = async (title, description, tag) => {
+        // ADD API Call
+        const response = await fetch(`${host}/api/notes/addnotes`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                //auth-token when client login in iNotebook
+                "auth-token": localStorage.getItem('token')
+            },
+            body: JSON.stringify({ title, description, tag })
+        });
+        const note = await response.json();
+        setNotes(notes.concat(note));
+    }
+
+    // Delete Notes
+    const deleteNote = async (id) => {
+        // DELETE API Call
+        const response = await fetch(`${host}/api/notes/deletenotes/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                //auth-token when client login in iNotebook
+                "auth-token": localStorage.getItem('token')
+            },
+        });
+        const json = await response.json();
+        console.log(json);
+
+        console.log("Delete notes with id " + id);
+        const newnotes = notes.filter((note) => { return note._id !== id })
+        setNotes(newnotes);
+    }
+
+    // Edit Notes
+    const editNote = async (id, title, description, tag) => {
+        // Edit API Call
+        const response = await fetch(`${host}/api/notes/updatenotes/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                //auth-token when client login in iNotebook
+
+                "auth-token": localStorage.getItem('token')
+            },
+            body: JSON.stringify({ title, description, tag })
+        });
+        const json = await response.json();
+        console.log(json);
+
+        let newNotes = JSON.parse(JSON.stringify(notes))
+        for (let index = 0; index < notes.length; index++) {
+            const element = newNotes[index];
+            if (element._id === id) {
+                newNotes[index].title = title;
+                newNotes[index].description = description;
+                newNotes[index].tag = tag;
+                break;
+            }
+        }
+        setNotes(newNotes);
+    }
+
+    return (
+        <NoteContext.Provider value={{ notes, setNotes, addNote, deleteNote, editNote, getNotes }}>
+            {props.children}
+        </NoteContext.Provider>
+    )
+}
+
+export default NoteState;
